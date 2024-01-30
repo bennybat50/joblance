16d670f93cdc8af9512b5384f15bcdccd27c2188
TamarauTokonih Standwell <tokonihstandwell@gmail.com>
2024-01-30 20:37:45 +0000

    update

--- frontend/src/common/config.js
+++ frontend/src/common/config.js
@@ -1,8 +1,8 @@
 
 //https://joblance-server.onrender.com/
 module.exports={
-    BASEURL:"http://localhost:7300"
+    // BASEURL:"http://localhost:7300"
 
-    // BASEURL:"https://joblance-server.onrender.com",
+    BASEURL:"https://joblance-server.onrender.com"
     // BASEURL:"http://localhost:7300"
 }
