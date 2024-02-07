caa2667433c9e1f02593c4a803ef715dc7709048
Young Batimehin <benny.bat51@gmail.com>
2024-02-07 11:11:19 +0100

    sdf

--- frontend/src/common/config.js
+++ frontend/src/common/config.js
@@ -3,6 +3,6 @@
 module.exports={
     // BASEURL:"http://localhost:7300"
 
-    //BASEURL:"https://joblance-server.onrender.com"
-    BASEURL:"http://localhost:7300"
+    BASEURL:"https://joblance-server.onrender.com"
+    //BASEURL:"http://localhost:7300"
 }
